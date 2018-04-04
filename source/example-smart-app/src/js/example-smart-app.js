20d7aa9e4d0bef9b8c90ba45f18abfc9378cb48b
wcsherb <38059097+wcsherb@users.noreply.github.com>
2018-04-04 09:47:22 -0400

    Update example-smart-app.js

--- source/example-smart-app/src/js/example-smart-app.js
+++ source/example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4'
+                              'http://loinc.org|29463-7']
                       }
                     }
                   });
@@ -42,6 +43,7 @@
           }
 
           var height = byCodes('8302-2');
+          var weight = byCodes('29463-7')'
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -54,6 +56,7 @@
           p.lname = lname;
           p.age = parseInt(calculateAge(dob));
           p.height = getQuantityValueAndUnit(height[0]);
+          p.weight = getQuantityValueAndUnit(weight[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -86,6 +89,7 @@
       birthdate: {value: ''},
       age: {value: ''},
       height: {value: ''},
+      weight: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
@@ -151,6 +155,7 @@
     $('#birthdate').html(p.birthdate);
     $('#age').html(p.age);
     $('#height').html(p.height);
+    $('#weight').html(p.weight);
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
